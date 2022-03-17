72c74b9cf1b299ae7c5e1a99cc3c57f19fe2b169
Kevin Gauvain <kevin.gauvain@decathlon.com>
2022-03-17 19:13:30 +0100

    update Input avancée

--- src/components/Mental_Card/Input_Form_Card.tsx
+++ src/components/Mental_Card/Input_Form_Card.tsx
@@ -1,62 +1,102 @@
 import React from "react";
 
 const InputCard = (props: any) => {
-  const [showingOneFaceOfCard, setShowingOneFaceOfCard] = React.useState(
-    props.product.questionTwo.question
-  );
-  const [text, setText] = React.useState("");
-  const [turn, setTurn] = React.useState(true);
+  const [testing, setTesting] = React.useState(true);
 
-  function handleInputText(data: any) {
-    if (data.target.value === props.product.questionTwo.answer) {
-      return <p>{props.product.questionTwo.answer}</p>;
-    } else {
-      const textWrong = "Wrong !";
-      return <p>{textWrong}</p>;
-    }
+  function test() {
+    return (
+      <div>
+        <label htmlFor="firstNameString">Right</label>
+        <p>Right ! Good answer !</p>
+      </div>
+    );
   }
 
-  const firstFaceOfCard = () => {
+  const test2 = () => {
     return (
       <div>
-        <label htmlFor="firstNameString">
-          {props.product.questionTwo.question}
-        </label>
-        <form>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Type your answer"
-          />
-          <button
-            className="btn btn-primary"
-            onSubmit={(event) => handleInputText(event)}
-          >
-            Submit
-          </button>
-        </form>
+        <label htmlFor="firstNameString">Wrong</label>
+        <p>Wrong ! Not the good answer !</p>
       </div>
     );
   };
 
-  const secondFaceOfCard = () => {
-    return <div>{handleInputText}</div>;
-  };
-
-  const finalCardTurn = () => {
-    return (
-      <div>
-        <button onClick={() => setTurn(!turn)}>
-          {turn ? firstFaceOfCard() : secondFaceOfCard()}
-          {/* <p>{props.question}</p> */}
+  return (
+    <div>
+      <label htmlFor="firstNameString">
+        {props.product.questionTwo.question}
+      </label>
+      <form>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Type your answer"
+        />
+        <button
+          className="btn btn-primary"
+          onClick={() => setTesting(!testing)}
+        >
+          Submit
         </button>
-      </div>
-    );
-  };
-
-  const final = finalCardTurn();
-  return <div>{final}</div>;
+        {testing ? <p>{test()}</p> : <p>{test2()}</p>}
+      </form>
+    </div>
+  );
 };
+
+//   const [showingOneFaceOfCard, setShowingOneFaceOfCard] = React.useState(
+//     props.product.questionTwo.question
+//   );
+//   const [text, setText] = React.useState("");
+//   const [turn, setTurn] = React.useState(true);
+
+//   function handleInputText(data: any) {
+//     if (data.target.value === props.product.questionTwo.answer) {
+//       return <p>vjvhjvj</p>;
+//     } else {
+//       return <p>nknllknlk</p>;
+//     }
+//   }
+
+//   // const firstFaceOfCard = () => {
+//   return (
+//     <div>
+//       <label htmlFor="firstNameString">
+//         {props.product.questionTwo.question}
+//       </label>
+//       <form onClick={(event) => handleInputText(event)}>
+//         <input
+//           type="text"
+//           className="form-control"
+//           placeholder="Type your answer"
+//         />
+//         <button
+//           className="btn btn-primary"
+//           onClick={(event) => handleInputText(event)}
+//         >
+//           Submit
+//         </button>
+//       </form>
+//     </div>
+//   );
+// };
+
+//   const secondFaceOfCard = () => {
+//     return <div>{firstFaceOfCard()}</div>;
+//   };
+
+//   const finalCardTurn = () => {
+//     return (
+//       <div>
+//         {turn ? firstFaceOfCard() : secondFaceOfCard()}
+//         {/* <p>{props.question}</p> */}
+//       </div>
+//     );
+//   };
+
+//   const final = finalCardTurn();
+//   return <div>{final}</div>;
+// };
 // const [faceRightOrWrong, setfaceRightOrWrong] = React.useState(true);
 
 // const secondFaceOfCard = () => {
